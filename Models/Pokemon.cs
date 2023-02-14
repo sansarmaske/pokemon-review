bb1814fab1c66c08bba318aa6b9fcf782d424f8c
Sansar Maske <sansarmaske@gmail.com>
2023-02-14 13:12:13 +0545

    chore: define many to many relationship

---
+++ Models/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿namespace PokemonReview.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+        public ICollection<Review> Reviews { get; }
+        public ICollection<PokemonOwner> PokemonOwner { get; set; }
+        public ICollection<PokemonCategory> PokemonCategory { get; set; }
+
+         
+    }
+}
