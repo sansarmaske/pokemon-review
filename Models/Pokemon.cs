a6bddb84d47c6773960f2cea7b42fb1df072e98f
Sansar Maske <sansarmaske@gmail.com>
2023-02-14 14:10:54 +0545

    chore: add seed file

--- Models/Pokemon.cs
+++ Models/Pokemon.cs
@@ -5,10 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string BirthDate { get; set; }
-        public ICollection<Review> Reviews { get; }
+        public ICollection<Review> Reviews { get; set; }
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
         public ICollection<PokemonCategory> PokemonCategories { get; set; }
 
-         
+        
     }
 }
