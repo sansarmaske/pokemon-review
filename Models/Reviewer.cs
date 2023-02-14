f6de3b1d2985b87d468799da049a1c164b5c7645
Sansar Maske <sansarmaske@gmail.com>
2023-02-14 12:58:27 +0545

    chore: define one to many relationship

--- Models/Reviewer.cs
+++ Models/Reviewer.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+        public ICollection<Review> Reviews { get; set;}
     }
 }
